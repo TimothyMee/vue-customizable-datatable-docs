d8f91b2037469f0d1dcbddc5fda5f2228a2c0ee2
TimothyMee <Timothy33.tf@gmail.com>
2020-04-27 13:51:21 +0100

    adding some new side nav styles

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -27,44 +27,7 @@
 
     <div class="col-md-12 row">
         <div class="col-md-4">
-          <div class="wrapper">
-            <!-- Sidebar -->
-            <nav id="sidebar">
-                <div class="sidebar-header">
-                    <h3 style="color:white">Table of Content</h3>
-                </div>
-
-                <ul class="list-unstyled components">
-                  <li :class="active === 'installation'? 'active': ''">
-                        <a href="#installation" @click="navigate('installation')">Installation & Usage</a>
-                    </li>
-                    <li :class="active === 'basic'? 'active': ''">
-                        <a href="#basic-table" @click="navigate('basic')">Basic Table</a>
-                    </li>
-                    <li :class="active === 'clickable'? 'active': ''">
-                        <a href="#clickable-table" @click="navigate('clickable')">Clickable Table</a>
-                    </li>
-                    <li :class="active === 'minimal'? 'active': ''">
-                        <a href="#minimal-table" @click="navigate('minimal')">Minimal Table</a>
-                    </li>
-                    <li :class="active === 'buttons'? 'active': ''">
-                        <a href="#buttons-table" @click="navigate('buttons')">Table With Buttons</a>
-                    </li>
-                    <li :class="active === 'select'? 'active': ''">
-                        <a href="#select-table" @click="navigate('select')">Select Table</a>
-                    </li>
-                    <li :class="active === 'customized'? 'active': ''">
-                        <a href="#customized-table" @click="navigate('customized')">Customized Table</a>
-                    </li>
-                    <li :class="active === 'loading'? 'active': ''">
-                        <a href="#loading-animation-table" @click="navigate('loading')">Loading Animation Table</a>
-                    </li>
-                    <li :class="active === 'paginated'? 'active': ''">
-                        <a href="#custom-paginated-table" @click="navigate('paginated')">Custom Paginated Table</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+          <side-nav></side-nav>
         </div>
         <div class="col-md-8">
           <h1>Vue Customizable DataTable</h1>
@@ -155,6 +118,7 @@ import TableWithCheckboxes from './TableWithCheckboxes';
 import CustomizedTable from './CustomizedTable';
 import LoadingAnimationTable from './LoadingAnimationTable';
 import CustomPaginationTable from './CustomPaginationTable';
+import SideNav from './SideNav';
 import VueMarkdown from 'vue-markdown';
 
 
@@ -171,6 +135,7 @@ export default {
     CustomizedTable,
     LoadingAnimationTable,
     CustomPaginationTable,
+    SideNav,
     VueMarkdown
   },
   data() {
---
+++ src/components/SideNav.vue
@@ -0,0 +1,46 @@
+<template>
+        <div class="wrapper">
+            <!-- Sidebar -->
+            <nav id="sidebar">
+                <div class="sidebar-header">
+                    <h3 style="color:white">Table of Content</h3>
+                </div>
+
+                <ul class="list-unstyled components">
+                  <li :class="active === 'installation'? 'active': ''">
+                        <a href="#installation" @click="navigate('installation')">Installation & Usage</a>
+                    </li>
+                    <li :class="active === 'basic'? 'active': ''">
+                        <a href="#basic-table" @click="navigate('basic')">Basic Table</a>
+                    </li>
+                    <li :class="active === 'clickable'? 'active': ''">
+                        <a href="#clickable-table" @click="navigate('clickable')">Clickable Table</a>
+                    </li>
+                    <li :class="active === 'minimal'? 'active': ''">
+                        <a href="#minimal-table" @click="navigate('minimal')">Minimal Table</a>
+                    </li>
+                    <li :class="active === 'buttons'? 'active': ''">
+                        <a href="#buttons-table" @click="navigate('buttons')">Table With Buttons</a>
+                    </li>
+                    <li :class="active === 'select'? 'active': ''">
+                        <a href="#select-table" @click="navigate('select')">Select Table</a>
+                    </li>
+                    <li :class="active === 'customized'? 'active': ''">
+                        <a href="#customized-table" @click="navigate('customized')">Customized Table</a>
+                    </li>
+                    <li :class="active === 'loading'? 'active': ''">
+                        <a href="#loading-animation-table" @click="navigate('loading')">Loading Animation Table</a>
+                    </li>
+                    <li :class="active === 'paginated'? 'active': ''">
+                        <a href="#custom-paginated-table" @click="navigate('paginated')">Custom Paginated Table</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
