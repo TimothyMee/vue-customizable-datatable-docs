233cab461e6bf45aa7a0046efb27178842caaf70
TimothyMee <Timothy33.tf@gmail.com>
2020-04-27 17:07:08 +0100

    fixed responsiveness for good. I think

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,48 +1,55 @@
 <template>
         <div class="menu">
             <!-- Sidebar -->
-            <nav id="sidebar">
-                <div class="navbar navbar-expand-lg navbar-light bg-light">
-                    <button type="button" id="sidebarCollapse" class="btn custom-blue">
-                        <svg class="h-6 w-10 fill-current text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            
+            <div id="sidebarButton">
+                <div style="width:40px;">
+                    <a href="#!" @click="toggleSidenav">
+                        <svg class="h-6 w-10" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 91.914 91.914">
                             <path
-                                d="M438 0H74C33.196 0 0 33.196 0 74s33.196 74 74 74h364c40.804 0 74-33.196 74-74S478.804 0 438 0zm0 108H74c-18.748 0-34-15.252-34-34s15.252-34 34-34h364c18.748 0 34 15.252 34 34s-15.252 34-34 34zM438 182H74c-40.804 0-74 33.196-74 74s33.196 74 74 74h364c40.804 0 74-33.196 74-74s-33.196-74-74-74zm0 108H74c-18.748 0-34-15.252-34-34s15.252-34 34-34h364c18.748 0 34 15.252 34 34s-15.252 34-34 34zM438 364H74c-40.804 0-74 33.196-74 74s33.196 74 74 74h264c11.046 0 20-8.954 20-20s-8.954-20-20-20H74c-18.748 0-34-15.252-34-34s15.252-34 34-34h364c18.748 0 34 15.252 34 34s-15.252 34-34 34c-11.046 0-20 8.954-20 20s8.954 20 20 20c40.804 0 74-33.196 74-74s-33.196-74-74-74z" />
+                                d="M1.124 0H90.79v14.089H1.124zM26.29 25.494h64.447v14.09H26.29zM1.124 52.33H90.79v14.09H1.124zM26.29 77.825h64.447v14.089H26.29z" />
                         </svg>
-                    </button>
+                    </a>
                 </div>
-                <div class="sidebar-header">
-                    <h3 style="color:white">Table of Content</h3>
-                </div>
+            </div>
 
-                <ul class="list-unstyled components">
-                  <li :class="active === 'installation'? 'active': ''">
-                        <a href="#installation" @click="navigate('installation')">Installation & Usage</a>
-                    </li>
-                    <li :class="active === 'basic'? 'active': ''">
-                        <a href="#basic-table" @click="navigate('basic')">Basic Table</a>
-                    </li>
-                    <li :class="active === 'clickable'? 'active': ''">
-                        <a href="#clickable-table" @click="navigate('clickable')">Clickable Table</a>
-                    </li>
-                    <li :class="active === 'minimal'? 'active': ''">
-                        <a href="#minimal-table" @click="navigate('minimal')">Minimal Table</a>
-                    </li>
-                    <li :class="active === 'buttons'? 'active': ''">
-                        <a href="#buttons-table" @click="navigate('buttons')">Table With Buttons</a>
-                    </li>
-                    <li :class="active === 'select'? 'active': ''">
-                        <a href="#select-table" @click="navigate('select')">Select Table</a>
-                    </li>
-                    <li :class="active === 'customized'? 'active': ''">
-                        <a href="#customized-table" @click="navigate('customized')">Customized Table</a>
-                    </li>
-                    <li :class="active === 'loading'? 'active': ''">
-                        <a href="#loading-animation-table" @click="navigate('loading')">Loading Animation Table</a>
-                    </li>
-                    <li :class="active === 'paginated'? 'active': ''">
-                        <a href="#custom-paginated-table" @click="navigate('paginated')">Custom Paginated Table</a>
-                    </li>
-                </ul>
+            <nav id="sidebar" :class="!sideNavActive ? 'active': ''">
+                <div id="sidebarContent">
+                    <div class="sidebar-header">
+                        <h3 style="color:white">Table of Content</h3>
+                    </div>
+
+                    <ul class="list-unstyled components">
+                    <li :class="active === 'installation'? 'active': ''">
+                            <a href="#installation" @click="navigate('installation')">Installation & Usage</a>
+                        </li>
+                        <li :class="active === 'basic'? 'active': ''">
+                            <a href="#basic-table" @click="navigate('basic')">Basic Table</a>
+                        </li>
+                        <li :class="active === 'clickable'? 'active': ''">
+                            <a href="#clickable-table" @click="navigate('clickable')">Clickable Table</a>
+                        </li>
+                        <li :class="active === 'minimal'? 'active': ''">
+                            <a href="#minimal-table" @click="navigate('minimal')">Minimal Table</a>
+                        </li>
+                        <li :class="active === 'buttons'? 'active': ''">
+                            <a href="#buttons-table" @click="navigate('buttons')">Table With Buttons</a>
+                        </li>
+                        <li :class="active === 'select'? 'active': ''">
+                            <a href="#select-table" @click="navigate('select')">Select Table</a>
+                        </li>
+                        <li :class="active === 'customized'? 'active': ''">
+                            <a href="#customized-table" @click="navigate('customized')">Customized Table</a>
+                        </li>
+                        <li :class="active === 'loading'? 'active': ''">
+                            <a href="#loading-animation-table" @click="navigate('loading')">Loading Animation Table</a>
+                        </li>
+                        <li :class="active === 'paginated'? 'active': ''">
+                            <a href="#custom-paginated-table" @click="navigate('paginated')">Custom Paginated Table</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
 
 
@@ -77,12 +84,17 @@
 export default {
     data() {
         return {
-        active:"basic"
+            active:"basic",
+            sideNavActive: true
         };
     },
     methods: {
         navigate(name) {
         this.active = name;
+        },
+        toggleSidenav() {
+            this.sideNavActive = !this.sideNavActive
+            console.log(this.sideNavActive);
         }
     },
     mounted() {
